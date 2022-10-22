c120f6ae814c3a1cb5e8207837a3aef66c9fbfe8
Alvaro Fuentes <afuentes2910@gmail.com>
2022-10-22 20:04:51 -0300

    ultima modif homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,13 +1,13 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "hola";
+const nuevaString = "holas";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 23;
 
-// Crea una variable booleana:
-const nuevoBool = true;
+// Crea una variable booleana:  
+const nuevoBool = true;  
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
@@ -15,7 +15,7 @@ const nuevaResta = 10 - 5 === 5;
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
-// Resuelve el siguiente problema matemático:
+// Resuelve el siguiente problema matemático: 
 const nuevoModulo = 21 % 5 === 1;
 
 
